c24c59c7da45670d0741fe4d7e1ebf683411829a
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-13 17:57:32 +0100

    tasks add

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-tasks = [ "Task1, "Task2", "Task3",]
+tasks = [ "Task1", "Task2", "Task3"]
 
 # Create your views here.
 def index(request):
     return render (request, "tasks/index.html", {
         "tasks": tasks
     })
+def add(request):
+    return render(request, "tasks/add.html")

c27598127989bcd98fd34af94a38308f5e7a4f1a
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-13 20:06:05 +0100

    forms django

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = [ "Task1", "Task2", "Task3"]
 
+class NewTaskForm(forms.form):
+    task =forms.CharField(lable="New Task")
 # Create your views here.
 def index(request):
     return render (request, "tasks/index.html", {
         "tasks": tasks
     })
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html"), {
+        "form": NewTaskForm()
+    }

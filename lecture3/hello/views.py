4abd0c10ad9e99d9459117c18c5296a933e6181c
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-13 12:17:42 +0100

    trying to fix the coding in index

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
 def kareem(request):
     return HttpResponse("Hello, Kareem!")
 def greet(request, name):

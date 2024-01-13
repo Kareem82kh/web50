319b5bdd7646087c544ab55eee9f75a79e695573
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-13 20:12:09 +0100

    form

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -4,13 +4,13 @@ from django import forms
 tasks = [ "Task1", "Task2", "Task3"]
 
 class NewTaskForm(forms.Form):
-    task =forms.CharField(label="New Task")
+    task = forms.CharField(label="New Task")
 # Create your views here.
 def index(request):
     return render (request, "tasks/index.html", {
         "tasks": tasks
     })
 def add(request):
-    return render(request, "tasks/add.html"), {
+    return render(request, "tasks/add.html", {
         "form": NewTaskForm()
-    }
+    })

f378dddb95fe17a66954114098dd1e2d392807b9
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-13 20:23:58 +0100

    tasks

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -5,13 +5,18 @@ tasks = [ "Task1", "Task2", "Task3"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 # Create your views here.
 def index(request):
     return render (request, "tasks/index.html", {
         "tasks": tasks
     })
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.clean_data["task"]
+            tasks.append(task)
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })

6a93aa3e5524cde867753a6ae497d078a85e426b
Kareem82kh <128170312+Kareem82kh@users.noreply.github.com>
2024-01-14 13:27:42 +0100

    update

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -10,6 +10,7 @@ class NewTaskForm(forms.Form):
 def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
+        
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
     })
@@ -19,11 +20,9 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            request.session["tasks"].append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
     else:
-        form = NewTaskForm()
-
-    return render(request, "tasks/add.html", {
-        "form": form
+        return render(request, "tasks/add.html", {
+            "form": form
     })
